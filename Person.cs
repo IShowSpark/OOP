f35aec5c2db6739bcc9612d9cc599269afc31b05
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:40:45 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+        string surname;
+        string status;
+        int age;
+        int salary;
+        string name;
+        int height;
+        public Person() { }
+        public Person(string Surname)
+        {
+            surname = Surname;
+        }
+        public string Surname
+        {
+            set { if (surname == null) surname = value; }
+            get { return surname; }
+        }
+        public int Age
+        {
+            set
+            {
+                age = value;
+                if (age<7)
+                {
+                    status = "Kids";
+                }
+                else if (age < 17)
+                {
+                    status = "Teen";
+                }
+                else if (age < 24)
+                {
+                    status = "Student";
+                }
+                else
+                {
+                    status = "Adult";
+                }
+            }
+            get { return age; }
+        }
+        public string Status
+        {
+            get { return status; }
+        }
+        public string Name
+        {
+            set { if (name == null) name = value; }
+            get { return name; }
+        }
+        public int Height
+        {
+            set
+            {
+                height = value;
+                if (height < 140)
+                {
+                    status = "Short";
+                }
+                else if (height < 180)
+                {
+                    status = "Nprmal";
+                }
+                else
+                {
+                    status = "Over height";
+                }
+            }
+            get { return height; }
+        }
+        public int Salary
+        {
+            set
+            {
+                salary = value;
+                if (salary < 600)
+                {
+                    status = "Small salary";
+                }
+                else if (salary < 1200)
+                {
+                    status = "Medium salary";
+                }
+                else
+                {
+                    status = "BIG SALARY";
+                }
+            }
+            get { return salary; }
+        }
+        //public void Hello()
+        //{
+        //    Console.WriteLine("My surname is "+ surname);
+        //    Console.WriteLine($"I'm {age} and I'm {status}");
+        //}
+        public void Bye()
+        {
+            Console.WriteLine("My name is "+ name);
+            Console.WriteLine($"I'm {height} cm and my salary {salary}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person man = new Person();
+            man.Surname = "Zelenskij";
+            man.Age = 44;
+            man.Height = 180;
+            man.Salary = 1600;
+            Console.WriteLine($"{man.Surname} is {man.Age},{man.Height},{man.Salary}");
+            //man.Hello();
+            man.Bye();
+            Person[] people = new Person[1];
+            for (int i = 0; i < 1; i++)
+            {
+                people[i] = new Person();
+                Console.WriteLine("What's your surname?");
+                people[i].Surname = Console.ReadLine();
+                Console.WriteLine("How old are you?");
+                people[i].Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("What's your name?");
+                people[i].Name = Console.ReadLine();
+                Console.WriteLine("What's your height?");
+                people[i].Height = int.Parse(Console.ReadLine());
+                Console.WriteLine("What's your salary?");
+                people[i].Salary = int.Parse(Console.ReadLine());
+            }
+            foreach (Person person in people)
+            {
+                //person.Hello();
+                person.Bye();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
